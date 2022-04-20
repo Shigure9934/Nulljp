07b878c95a182d9e09082010039ad271967b0a2f
Shigure D <99520407+Shigure9934@users.noreply.github.com>
2022-04-20 20:25:15 -0300

    Main guide section and about added
    mindset section still on works

--- components/header.js
+++ components/header.js
@@ -5,18 +5,18 @@ import React from 'react';
     function Header() {
     return (
           <div className=''>
-        <header className=' underline flex justify-evenly  m-auto md-header_ellipsis text-slate-300 px-15 pt-5 pb-5' href="/index"> 
-        <button>
-            <Link className=' underline' href='/'>ヌル</Link>
+        <header className='  flex justify-evenly  m-auto md-header_ellipsis text-slate-300 px-15 pt-5 pb-5' href="/index"> 
+        <button  className=' hover:underline'>
+            <Link className='' href='/'>ヌル</Link>
         </button>
-        <button>
+        <button  className=' hover:underline'>
             <Link className=' underline' href='/about'>About</Link>
         </button>
         
-        <button>
+        <button  className=' hover:underline'>
         <Link className=' underline' href='/guide'>Guide</Link>
-        </button>
-        <button>
+        </button >
+        <button  className=' hover:underline'>
           <Link className=' underline' href='/resources'>Resources</Link>
         </button>
         </header>
--- pages/about.js
+++ pages/about.js
@@ -4,9 +4,14 @@ function About(){
     return (
     <div>
         <Header />
-            <h1 className=' flex text-3xl px-8 pt-12 underline'>
+            <h1 className=' flex text-3xl px-8 pt-12 pb-8 underline'>
                 About
             </h1>
+            <p className='flex text-xl px-8'>
+            This guide was created with the goal of helping people learn Japanese <br />
+            for free. Be it by watching TV shows, listening to music, or simply reading. <br />
+            All the content inside this guide is curated by the community and free. <br /> 
+                </p>
         </div>
         );
 }
