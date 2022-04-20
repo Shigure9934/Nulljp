e05be52ee31bb1963dd17258874aaa72dae2cc0e
Shigure D <99520407+Shigure9934@users.noreply.github.com>
2022-04-20 20:45:57 -0300

    Update guide.js

--- pages/guide.js
+++ pages/guide.js
@@ -10,8 +10,8 @@ function Guide(){
             </h1>
             <p className='text-sm px-8 pt-8'>
                 So, how do I learn Japanese? you may be asking yourself. <br />
-                Well, first of all you need to have the right <strong> mindset  </strong> before you dive into seriously learning Japanese. <br />
-                After all It' s something that takes a lot of time and effort, that's why this guide will start by guiding you into having the right mindset when it comes to learning Japanese. <br />                 
+                Well, first of all you need to have the right <strong> mindset  </strong> before you dive into seriously learning . <br />
+                After all It&apos;' s something that takes a lot of time and effort, that&apos;s why this guide will start by guiding you into having the right mindset when it comes to learning Japanese. <br />                 
             </p>
             <h2 className='flex text-3xl px-8 pt-12 underline'>
                 The Mindset 
