f8c878e67859d41df8a493c356582c1ec2528b0c
Shigure D <99520407+Shigure9934@users.noreply.github.com>
2022-04-23 18:58:44 -0300

    cleaned some code

--- pages/resources.js
+++ pages/resources.js
@@ -1,11 +1,15 @@
-import Header from '../components/header';
+import Header from "../components/header";
 
-function Resources(){ 
-    return (
+function Resources() {
+  return (
     <div>
-        <Header />
-        </div>
-        );
+      <Header />
+      <h1 className="flex text-3xl px-8 pt-8 underline">Resources</h1>
+      <p className="text-sm px-8 pt-8">
+        I'll now list some resources that will help you learn Japanese.
+      </p>
+    </div>
+  );
 }
 
 export default Resources;
