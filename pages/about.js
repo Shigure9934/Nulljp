6c8a9a1e0c22b484a31b761b6a6bd368e3b2247b
Shigure D <dadasax@protonmail.com>
2022-05-22 11:11:49 -0300

    about section expanded

--- pages/about.js
+++ pages/about.js
@@ -19,11 +19,15 @@ visible: {
   }
 },
 }}> 
-            <h1 className=' fflex text-3xl px-8 pt-12 underline'>
+            <h1 className=' flex text-3xl px-8 pt-12 underline'>
                 About
             </h1>
-            <p className='flex text-sm px-8 pt-8'>This website was created with the intention of helping people learn japanese, 
-            We are a community of learners sharing our methods and explaining the process, giving resources, etc.</p>
+            <p className='flex text-xl px-8 pt-8'>This website was created with the intention of helping people learn japanese, 
+            We are a community of learners sharing our methods and explaining the process, giving resources, etc.
+            Our community is made mainly by self taught learners of not only the japanese language but some more languages as well.
+            </p>
+            <p className='flex text-xl px-8 pt-8'>
+              Our method is commonly known as immersion learning, and 
             <p className='flex px-5 pb-1 bottom-0 fixed text-xs	'>
   Github- Shigure9934 
 </p>
